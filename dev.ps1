c8e9018b5e87f536299b488843b073cb61c75dfc
i314q159 <i314q159@outlook.com>
2023-07-22 13:44:00 +0800

    update

---
+++ dev.ps1
@@ -0,0 +1,4 @@
+pip install pyinstaller
+pyinstaller -F scoop-gui.py
+
+pip freeze > requirements.txt
---
+++ scoop-gui.py
@@ -0,0 +1,31 @@
+import concurrent.futures
+import json
+import os
+
+import tqdm
+
+scoop_json_path = "./scoop.json"
+
+with open(scoop_json_path, encoding="utf-16", newline="\n") as f:
+    data = json.load(f)
+
+apps = data["apps"]
+
+commands = []
+for app in apps:
+    name = app["Name"]
+    source = app["Source"]
+    cmd = f"scoop install {source}/{name}"
+    commands.append(cmd)
+
+
+def run_cmd(cmd):
+    os.system(cmd)
+
+
+with tqdm.tqdm(total=len(commands)) as pbar:
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        for result in executor.map(run_cmd, commands):
+            pbar.update()
+
+print("All scoop apps installed")
