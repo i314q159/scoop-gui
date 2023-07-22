0b6a22534c8d38ceb2d609ad7550095ba7f421b6
i314q159 <i314q159@outlook.com>
2023-07-22 22:03:05 +0800

    add max_workers

--- scoop-import.py
+++ scoop-import.py
@@ -7,6 +7,9 @@ import tqdm
 
 scoop_json_path = sys.argv[1]
 
+cpu_count = os.cpu_count() or 4 
+max_workers = cpu_count * 3
+
 with open(scoop_json_path, encoding="utf-16", newline="\n") as f:
     data = json.load(f)
 
@@ -25,7 +28,7 @@ def run_cmd(cmd):
 
 
 with tqdm.tqdm(total=len(commands)) as pbar:
-    with concurrent.futures.ThreadPoolExecutor() as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_workers) as executor:
         for result in executor.map(run_cmd, commands):
             pbar.update()
 
